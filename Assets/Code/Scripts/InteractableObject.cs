61fafee0badbbce0752502b7b21a08257d71a581
IrvingMacias <53436577+IrvingMaciasV@users.noreply.github.com>
2023-09-08 23:52:45 -0600

    Interaction Objects
    Features: New Prefab for the interactions with the objects
    Bug: Panel with text

--- Assets/Code/Scripts/InteractableObject.cs
+++ Assets/Code/Scripts/InteractableObject.cs
@@ -11,9 +11,9 @@ namespace Code.Scripts
         [Header("General")]
         [SerializeField] InventoryClass inventoryClass;
         [SerializeField] bool hasPanel;
-        Canvas canvas;
         GameObject panel;
         GameObject UIElement;
+        [SerializeField] Vector3 originalPos;
 
         [Header ("Drag and Drop")]
         Sprite sprite;
@@ -26,6 +26,7 @@ namespace Code.Scripts
             if (UIElement == null)
                 UIElement = GameObject.Find("UI Element");
             sprite = GetComponent<SpriteRenderer>().sprite;
+            originalPos = this.transform.position;
         }
 
         private void OnMouseDown()
@@ -40,7 +41,7 @@ namespace Code.Scripts
                 mOffset = gameObject.transform.position - GetMouseWorldPosition();
                 this.GetComponent<SpriteRenderer>().enabled = false;
                 UIElement.GetComponent<Image>().sprite = sprite;
-                UIElement.SetActive(true);
+                UIElement.GetComponent<Image>().enabled = true;
             }
         }
 
@@ -67,30 +68,47 @@ namespace Code.Scripts
 
         private void OnMouseUp()
         {
-            Debug.Log("MouseUp");
-
-            //RaycastHit2D hit = Physics2D.CircleCast(UIElement.transform.position + (Vector3)deltaPos, radius, UIElement.transform.right, distance);
+            UIElement.GetComponent<CircleCollider2D>().enabled = true;
             RaycastHit2D[] hit = Physics2D.CircleCastAll(UIElement.transform.position, UIElement.GetComponent<CircleCollider2D>().radius, Vector2.zero);
             foreach (RaycastHit2D h in hit)
             {
                 if(h.transform.GetComponent<InventoryItem>())
                 {
-                    
+                    if (CheckCollision(h.transform.GetComponent<InventoryItem>()))
+                    {
+                        AproveItem();
+                    }
+                    ReturnItem();
+                    return;
                 }
             }
+
+            ReturnItem();
         }
 
-        private void CheckCollision(InventoryItem item)
+        private bool CheckCollision(InventoryItem item)
         {
             if (item.inventoryClass == inventoryClass)
             {
-
+                return true;
             }
+            return false;
+        }
+
+        private void AproveItem()
+        {
+            UIElement.GetComponent<Image>().enabled = false;
+            Destroy(this.gameObject);
+            UIElement.GetComponent<CircleCollider2D>().enabled = false;
         }
 
         private void ReturnItem()
         {
-            UIElement.SetActive(false);
+            UIElement.GetComponent<CircleCollider2D>().enabled = false;
+            UIElement.GetComponent<Image>().enabled = false;
+            UIElement.transform.position = Vector3.zero;
+            this.transform.position = originalPos;
+            this.GetComponent<SpriteRenderer>().enabled = true;
         }
 
         private void OnDrawGizmos()
