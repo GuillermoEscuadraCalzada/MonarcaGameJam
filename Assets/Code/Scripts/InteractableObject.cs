78e519cbde28021d15fddd729c6635a1b116fde4
IrvingMacias <53436577+IrvingMaciasV@users.noreply.github.com>
2023-09-08 19:17:43 -0600

    Collisions on UI Drag
    Description; Can drag an item to a UI
    Bug; Return the Item to original position

--- Assets/Code/Scripts/InteractableObject.cs
+++ Assets/Code/Scripts/InteractableObject.cs
@@ -2,26 +2,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Code.Scripts
 {
-    public class InteractableObject : MonoBehaviour, IPointerEnterHandler
+    public class InteractableObject : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [Header("General")]
+        [SerializeField] InventoryClass inventoryClass;
+        [SerializeField] bool hasPanel;
+        Canvas canvas;
+        GameObject panel;
+        GameObject UIElement;
+
+        [Header ("Drag and Drop")]
+        Sprite sprite;
+        private Vector3 mOffset;
+
+        private void Start()
         {
-            
+            if (panel == null)
+                panel = GameObject.Find("Panel Description");
+            if (UIElement == null)
+                UIElement = GameObject.Find("UI Element");
+            sprite = GetComponent<SpriteRenderer>().sprite;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnMouseDown()
         {
-            
+            if (hasPanel)
+            {
+                panel.SetActive(true);
+            }
+
+            else
+            {
+                mOffset = gameObject.transform.position - GetMouseWorldPosition();
+                this.GetComponent<SpriteRenderer>().enabled = false;
+                UIElement.GetComponent<Image>().sprite = sprite;
+                UIElement.SetActive(true);
+            }
         }
-        
-        public void OnPointerEnter(PointerEventData eventData)
+
+        private Vector3 GetMouseWorldPosition()
         {
-            print("Pointer entered");
+            Vector3 mousePoint=Input.mousePosition;
+            mousePoint.z = 0;
+            return Camera.main.ScreenToWorldPoint(mousePoint);
+        }
+
+        private void OnMouseDrag()
+        {
+
+            if (!hasPanel)
+            {
+                transform.position = GetMouseWorldPosition() + mOffset;
+                
+                Vector3 mousePos=Input.mousePosition;
+                float x = mousePos.x;
+                float y = mousePos.y;
+                UIElement.transform.position = new Vector3(x, y, 0);
+            }
+        }
+
+        private void OnMouseUp()
+        {
+            Debug.Log("MouseUp");
+
+            //RaycastHit2D hit = Physics2D.CircleCast(UIElement.transform.position + (Vector3)deltaPos, radius, UIElement.transform.right, distance);
+            RaycastHit2D[] hit = Physics2D.CircleCastAll(UIElement.transform.position, UIElement.GetComponent<CircleCollider2D>().radius, Vector2.zero);
+            foreach (RaycastHit2D h in hit)
+            {
+                if(h.transform.GetComponent<InventoryItem>())
+                {
+                    
+                }
+            }
+        }
+
+        private void CheckCollision(InventoryItem item)
+        {
+            if (item.inventoryClass == inventoryClass)
+            {
+
+            }
+        }
+
+        private void ReturnItem()
+        {
+            UIElement.SetActive(false);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
         }
     }
 }
